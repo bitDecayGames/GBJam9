3f565dfaf75a8efffed026210f158adebb3ed122
Logan <monday.hopscotch@gmail.com>
2021-09-26 12:43:03 -0600

    prevent negative points

--- source/metrics/Trackers.hx
+++ source/metrics/Trackers.hx
@@ -2,5 +2,9 @@ package metrics;
 
 class Trackers {
 	public static var attemptTimer = 0.0;
-	public static var points = 0;
+	public static var points(default, set) = 0;
+
+	static function set_points(newP) {
+		return newP < 0 ? 0 : newP;
+	}
 }
