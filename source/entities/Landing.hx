94de4dcc968b0a54488502697a211fef4cd6f82b
Logan <monday.hopscotch@gmail.com>
2021-09-27 17:03:41 -0600

    small level cleanup. add rating particle for landing

--- source/entities/Landing.hx
+++ source/entities/Landing.hx
@@ -1,5 +1,10 @@
 package entities;
 
+import states.PlayState;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import ui.font.BitmapText.AerostatRed;
+import flixel.FlxG;
 import flixel.util.FlxColor;
 import flixel.FlxSprite;
 
@@ -18,22 +23,48 @@ class Landing extends FlxSprite {
 	}
 
 	public function getScore(landX:Float):Int {
+		var score = 0;
+
 		var distanceFromFront = landX - x;
+		#if debug
 		trace('landing distance: ${distanceFromFront}');
+		#end
 		if (distanceFromFront < 8) {
 			// first tile is a "bad" zone
 			// landed before the scoring zone
 			#if debug
 			trace('missed landing');
 			#end
-			return 100;
+			score = 100;
 		} else {
 			// subtract one as the 'perfect' distance is actually one tile back
 			var deduction = 100 * Math.floor(distanceFromFront / 8 - 1);
 			#if debug
 			trace('landing deduction: ${deduction}');
 			#end
-			return 1000 - deduction;
+			score = 1000 - deduction;
 		}
+
+		var rating = "F";
+		if (score == 1000) {
+			rating = "S";
+		} else if (score >= 800) {
+			rating = "A";
+		} else if (score >= 600) {
+			rating = "B";
+		} else if (score >= 400) {
+			rating = "C";
+		}
+
+		var display = new AerostatRed(landX - 4, y, rating);
+		FlxTween.linearMotion(display, display.x, display.y, display.x, display.y - 36, 1.5, {
+			ease: FlxEase.quadOut,
+			onComplete: (t) -> {
+				display.kill();
+			}
+		});
+		cast(FlxG.state, PlayState).addParticle(display);
+
+		return score;
 	}
 }
